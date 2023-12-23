90e931621ff56c796d75d97a53c6a451cc012123
dudaalenc4r <maria4.mnzs@gmail.com>
2023-12-23 20:07:30 -0300

    meu projeto conta banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -5,10 +5,10 @@ public class ContaTerminal {
     
     public static void main(String[] args) {
 
-        
+        // TODO: conhecer e importar a classe scanner
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        // determinando as variáveis e colhendo os dados do cliente
+        // Exibir as mensagens para o usuário e obter pelo scanner os valores digitados no terminal
 
         System.out.println("Digite seu primeiro nome: ");
         String nome = scanner.next();
@@ -22,9 +22,9 @@ public class ContaTerminal {
         System.out.println("Por favor, digite o saldo de sua conta: ");
         float saldo = scanner.nextFloat();
 
-        // imprimindo texto de retorno para o cliente
+        // imprimindo texto de retorno para o usuário
 
-        System.out.println("Olá, " + nome + ", " + "obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", " + "conta " + numero + ", " + "e seu saldo de " + saldo + " reais " + "já está disponível para saque");
+        System.out.println("Olá " + nome + ", " + "obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", " + "conta " + numero + ", " + "e seu saldo de " + saldo + " reais " + "já está disponível para saque.");
 
     }
 }
